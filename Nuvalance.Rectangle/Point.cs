1ebfc13334f18e0618e6e2734a20e91abac6a1f5
Andres Felipe Diaz Gomez <afdg25@hotmail.com>
2022-11-13 22:43:14 -0500

    Add project files.

---
+++ Nuvalance.Rectangle/Point.cs
@@ -0,0 +1,79 @@
+﻿using System.Diagnostics;
+
+namespace Nuvalance.Rectangle
+{
+    /// <summary>
+    /// Represents a point in a cartesian coordinate system.
+    /// </summary>
+    [DebuggerDisplay("X:{X}, Y:{Y}")]
+    public class Point : IEquatable<Point>
+    {
+        /// <summary>
+        /// Initialized a new <see cref="Point"/> class with the given coordinates.
+        /// </summary>
+        /// <param name="x">X axis coordinate.</param>
+        /// <param name="y">Y axis coordinate.</param>
+        public Point(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        /// <summary>
+        /// X axis coordinate.
+        /// </summary>
+        public int X { get; }
+
+        /// <summary>
+        /// Y axis coordinate.
+        /// </summary>
+        public int Y { get; }
+
+        /// <summary>
+        /// Indicates whether the current point is located on the same axis to another point.
+        /// </summary>
+        /// <param name="other">The other point to compare with.</param>
+        /// <returns>true if the current object is located on the same axis to the other parameter; otherwise, false.</returns>
+        public bool IsOnTheSameAxis(Point other)
+        {
+            if (other == null)
+            {
+                return false;
+            }
+            return X == other.X || Y == other.Y;
+        }
+
+        /// <summary>
+        /// Indicates whether the current point is equal to another point.
+        /// </summary>
+        /// <param name="other">The other point to compare with.</param>
+        /// <returns>true if the current object is equal to the other parameter; otherwise, false.</returns>
+        public bool Equals(Point other)
+        {
+            if (other == null)
+            {
+                return false;
+            }
+            return X == other.X && Y == other.Y;
+        }
+
+        /// <summary>
+        /// Indicates whether the current object is equal to another object.
+        /// </summary>
+        /// <param name="other">The other object to compare with this.</param>
+        /// <returns>true if the current object is equal to the other parameter; otherwise, false.</returns>
+        public override bool Equals(object obj)
+        {
+            return Equals(obj as Point);
+        }
+
+        /// <summary>
+        /// Returns the hash code for this instance.
+        /// </summary>
+        /// <returns>A 32-bit signed integer hash code.</returns>
+        public override int GetHashCode()
+        {
+            return X.GetHashCode() ^ Y.GetHashCode();
+        }
+    }
+}
---
+++ Nuvalance.Rectangle/Rectangle.cs
@@ -0,0 +1,158 @@
+﻿using System.Diagnostics;
+
+namespace Nuvalance.Rectangle
+{
+    /// <summary>
+    /// Representation of a rectangle in a cartesian coordinate system.
+    /// </summary>
+    [DebuggerDisplay("({PointA.X}, {PointA.Y}) ({PointC.X}, {PointC.Y})")]
+    public class Rectangle
+    {
+        public Rectangle(Point a, Point b)
+        {
+            int leftX = Math.Min(a.X, b.X);
+            int rightX = Math.Max(a.X, b.X);
+            int topY = Math.Max(a.Y, b.Y);
+            int bottomY = Math.Min(a.Y, b.Y);
+
+            PointA = new Point(leftX, topY);
+            PointB = new Point(rightX, topY);
+            PointC = new Point(rightX, bottomY);
+            PointD = new Point(leftX, bottomY);
+        }
+
+        /// <summary>
+        /// Left top corner point
+        /// </summary>
+        public Point PointA { get; }
+
+        /// <summary>
+        /// Right top corner point
+        /// </summary>
+        public Point PointB { get; }
+
+        /// <summary>
+        /// Right bottom corner point
+        /// </summary>
+        public Point PointC { get; }
+
+        /// <summary>
+        /// Left bottom corner point
+        /// </summary>
+        public Point PointD { get; }
+
+        /// <summary>
+        /// Get an intersection result comparing the current to another rectangle.
+        /// </summary>
+        /// <param name="other">Other rectangle to compare with.</param>
+        /// <returns>A IntersectionResult with the points of intersection.</returns>
+        public IntersectionResult GetIntersection(Rectangle other)
+        {
+            if (!AreIntersected(other))
+            {
+                return IntersectionResult.NoIntersection;
+            }
+            var pointA = new Point(Math.Max(PointA.X, other.PointA.X), Math.Min(PointA.Y, other.PointA.Y));
+            var pointC = new Point(Math.Min(PointC.X, other.PointC.X), Math.Max(PointC.Y, other.PointC.Y));
+            var rectangle = new Rectangle(pointA, pointC);
+            var points = new List<Point>();
+            if (IsPointOnAnyAxis(rectangle.PointA) && other.IsPointOnAnyAxis(rectangle.PointA))
+            {
+                points.Add(rectangle.PointA);
+            }
+            if (IsPointOnAnyAxis(rectangle.PointB) && other.IsPointOnAnyAxis(rectangle.PointB))
+            {
+                points.Add(rectangle.PointB);
+            }
+            if (IsPointOnAnyAxis(rectangle.PointC) && other.IsPointOnAnyAxis(rectangle.PointC))
+            {
+                points.Add(rectangle.PointC);
+            }
+            if (IsPointOnAnyAxis(rectangle.PointD) && other.IsPointOnAnyAxis(rectangle.PointD))
+            {
+                points.Add(rectangle.PointD);
+            }
+            return new IntersectionResult(points.Distinct());
+        }
+
+        /// <summary>
+        /// Indicates whether the given point is on the axis of the current rectangle.
+        /// </summary>
+        /// <param name="point">The point to compare with.</param>
+        /// <returns>true if the point is on any axis; otherwise, false.</returns>
+        public bool IsPointOnAnyAxis(Point point)
+        {
+            return PointA.IsOnTheSameAxis(point) ||
+                PointB.IsOnTheSameAxis(point) ||
+                PointC.IsOnTheSameAxis(point) ||
+                PointD.IsOnTheSameAxis(point);
+        }
+
+        private bool AreIntersected(Rectangle other)
+        {
+            if (other == null)
+            {
+                return false;
+            }
+
+            // if rectangle has no area
+            if (PointA.IsOnTheSameAxis(PointC) ||
+                other.PointA.IsOnTheSameAxis(other.PointC))
+            {
+                return false;
+            }
+
+            // If one rectangle is located on left side of the other
+            if (PointA.X > other.PointC.X ||
+                other.PointA.X > PointC.X)
+            {
+                return false;
+            }
+
+            // If one rectangle is located above the other
+            if (PointC.Y > other.PointA.Y ||
+                other.PointC.Y > PointA.Y)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// Represents the result of an intersection of two rectangles.
+        /// It contains the information of the found points.
+        /// </summary>
+        public class IntersectionResult
+        {
+            /// <summary>
+            /// No intersection result.
+            /// </summary>
+            public static IntersectionResult NoIntersection = new IntersectionResult();
+
+            /// <summary>
+            /// Initialized a new <see cref="IntersectionResult"/> class with the given points.
+            /// </summary>
+            /// <param name="interesectionPoints">The set of intersection points</param>
+            public IntersectionResult(IEnumerable<Point> interesectionPoints)
+            {
+                Intersections = interesectionPoints ?? new List<Point>();
+            }
+
+            private IntersectionResult()
+            {
+                Intersections = new List<Point>();
+            }
+
+            /// <summary>
+            /// List of intersections points.
+            /// </summary>
+            public IEnumerable<Point> Intersections { get; }
+
+            /// <summary>
+            /// Indicate wether or not the result is an intersection.
+            /// </summary>
+            public bool Result => Intersections.Any();
+        }
+    }
+}
